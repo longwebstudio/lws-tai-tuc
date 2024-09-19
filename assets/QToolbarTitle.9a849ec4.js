052b91b68af6b4088a7064e8552857303677fb36
longwebstudio <info@longwebstudio.net>
2024-09-19 09:19:11 +0000

    Deploying to gh-pages from @ longwebstudio/lws-tai-tuc@6e080f87f76e109b9949814f4a624b4a71e1c6c2 🚀

--- assets/QToolbarTitle.9a849ec4.js
+++
@@ -1,1 +0,0 @@
-import{c as e,a as t,b as l,H as r}from"./index.f11bd7c6.js";var n=e({name:"QToolbarTitle",props:{shrink:Boolean},setup(o,{slots:s}){const a=t(()=>"q-toolbar__title ellipsis"+(o.shrink===!0?" col-shrink":""));return()=>l("div",{class:a.value},r(s.default))}});export{n as Q};
